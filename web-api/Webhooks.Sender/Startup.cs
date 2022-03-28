c7a74a954af12b829b4ab401d900aeea400512f1
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2022-03-28 17:54:31 +0200

    Sender: minor cleanup

--- web-api/Webhooks.Sender/Startup.cs
+++ web-api/Webhooks.Sender/Startup.cs
@@ -4,9 +4,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Polly;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Polly;
 using Webhooks.Sender.DbContexts;
 
 namespace Webhooks.Sender
