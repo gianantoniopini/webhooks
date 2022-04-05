885c7c304220ec9633a0d32911cfa7bab92b3352
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2022-04-05 17:21:09 +0200

    Sender: migrated to ASP.NET Core 6.0

--- web-api/Webhooks.Sender/Startup.cs
+++ web-api/Webhooks.Sender/Startup.cs
@@ -1,11 +1,11 @@
 using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using Microsoft.EntityFrameworkCore;
 using Polly;
 using Webhooks.Sender.DbContexts;
 
