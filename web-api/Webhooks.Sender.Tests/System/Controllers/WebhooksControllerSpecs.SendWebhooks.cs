58ddec4bd5cc99d4888157769953e88ff69e2520
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2022-04-14 13:45:15 +0200

    Sender: added more tests

--- web-api/Webhooks.Sender.Tests/System/Controllers/WebhooksControllerSpecs.SendWebhooks.cs
+++ web-api/Webhooks.Sender.Tests/System/Controllers/WebhooksControllerSpecs.SendWebhooks.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using Moq;
 using Moq.Contrib.HttpClient;
 using Xunit;
 using Webhooks.Sender.Models;
@@ -7,6 +8,33 @@ namespace Webhooks.Sender.Tests.System.Controllers
 {
     public partial class WebhooksControllerSpecs
     {
+        [Fact]
+        public async Task SendWebhooks_ShouldIgnoreDisabledWebhooks()
+        {
+            // Arrange
+            var webhooks = new List<Webhook>
+            {
+                new Webhook { Id = 1, PayloadUrl = "https://url1.com", IsActive = true, CreatedAt = DateTimeOffset.UtcNow },
+                new Webhook { Id = 2, PayloadUrl = "https://url2.com", IsActive = false, CreatedAt = DateTimeOffset.UtcNow },
+                new Webhook { Id = 3, PayloadUrl = "https://url3.com", IsActive = true, CreatedAt = DateTimeOffset.UtcNow },
+            };
+            _dbContext.Webhooks.AddRange(webhooks);
+            _dbContext.SaveChanges();
+
+            _httpMessageHandler.SetupRequest(HttpMethod.Post, webhooks[0].PayloadUrl).ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK), TimeSpan.FromMilliseconds(100));
+            _httpMessageHandler.SetupRequest(HttpMethod.Post, webhooks[1].PayloadUrl).ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK), TimeSpan.FromMilliseconds(500));
+            _httpMessageHandler.SetupRequest(HttpMethod.Post, webhooks[2].PayloadUrl).ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK), TimeSpan.FromMilliseconds(150));
+
+            // Act
+            await _controller.SendWebhooks();
+
+            // Assert
+            _httpMessageHandler.VerifyAnyRequest(Times.Exactly(2));
+            _httpMessageHandler.VerifyRequest(HttpMethod.Post, webhooks[0].PayloadUrl, Times.Exactly(1));
+            _httpMessageHandler.VerifyRequest(HttpMethod.Post, webhooks[1].PayloadUrl, Times.Never());
+            _httpMessageHandler.VerifyRequest(HttpMethod.Post, webhooks[2].PayloadUrl, Times.Exactly(1));
+        }
+
         [Fact]
         public async Task SendWebhooks_IfRequestToAPayloadUrlFails_ShouldNotStopRequestsToOtherPayloadUrls()
         {
@@ -20,15 +48,18 @@ namespace Webhooks.Sender.Tests.System.Controllers
             _dbContext.Webhooks.AddRange(webhooks);
             _dbContext.SaveChanges();
 
-            _httpMessageHandler.SetupRequest(HttpMethod.Post, webhooks[0].PayloadUrl).ReturnsResponse(HttpStatusCode.OK);
-            _httpMessageHandler.SetupRequest(HttpMethod.Post, webhooks[1].PayloadUrl).ReturnsResponse(HttpStatusCode.InternalServerError);
-            _httpMessageHandler.SetupRequest(HttpMethod.Post, webhooks[2].PayloadUrl).ReturnsResponse(HttpStatusCode.OK);
+            _httpMessageHandler.SetupRequest(HttpMethod.Post, webhooks[0].PayloadUrl).ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK), TimeSpan.FromMilliseconds(100));
+            _httpMessageHandler.SetupRequest(HttpMethod.Post, webhooks[1].PayloadUrl).ReturnsAsync(new HttpResponseMessage(HttpStatusCode.InternalServerError), TimeSpan.FromMilliseconds(500));
+            _httpMessageHandler.SetupRequest(HttpMethod.Post, webhooks[2].PayloadUrl).ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK), TimeSpan.FromMilliseconds(150));
 
             // Act
             await _controller.SendWebhooks();
 
             // Assert
-            _httpMessageHandler.VerifyAll();
+            _httpMessageHandler.VerifyAnyRequest(Times.Exactly(3));
+            _httpMessageHandler.VerifyRequest(HttpMethod.Post, webhooks[0].PayloadUrl, Times.Exactly(1));
+            _httpMessageHandler.VerifyRequest(HttpMethod.Post, webhooks[1].PayloadUrl, Times.Exactly(1));
+            _httpMessageHandler.VerifyRequest(HttpMethod.Post, webhooks[2].PayloadUrl, Times.Exactly(1));
         }
     }
 }
