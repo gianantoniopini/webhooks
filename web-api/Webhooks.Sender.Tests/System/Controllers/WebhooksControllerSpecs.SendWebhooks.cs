3e4beaaa042f3be4b1564febcacaca3982158c0b
gianantoniopini <63844628+gianantoniopini@users.noreply.github.com>
2022-04-21 11:54:02 +0200

    Sender: added more tests

--- web-api/Webhooks.Sender.Tests/System/Controllers/WebhooksControllerSpecs.SendWebhooks.cs
+++ web-api/Webhooks.Sender.Tests/System/Controllers/WebhooksControllerSpecs.SendWebhooks.cs
@@ -1,9 +1,12 @@
+using System.Globalization;
 using System.Net;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Moq.Contrib.HttpClient;
+using Newtonsoft.Json;
 using Xunit;
 using Webhooks.Sender.Models;
+using Webhooks.Sender.ResourceModels;
 
 namespace Webhooks.Sender.Tests.System.Controllers
 {
@@ -34,6 +37,33 @@ namespace Webhooks.Sender.Tests.System.Controllers
             _httpMessageHandler.VerifyRequest(HttpMethod.Post, webhooks[0].PayloadUrl, Times.Once());
             _httpMessageHandler.VerifyRequest(HttpMethod.Post, webhooks[1].PayloadUrl, Times.Never());
             _httpMessageHandler.VerifyRequest(HttpMethod.Post, webhooks[2].PayloadUrl, Times.Once());
+        }
+
+        [Fact]
+        public async Task SendWebhooks_ShouldSendRequestsWithJsonPayload()
+        {
+            // Arrange
+            var webhooks = new List<Webhook>
+            {
+                new Webhook { Id = 1, PayloadUrl = "https://url1.com", IsActive = true, CreatedAt = DateTimeOffset.UtcNow },
+                new Webhook { Id = 2, PayloadUrl = "https://url2.com", IsActive = true, CreatedAt = DateTimeOffset.UtcNow },
+                new Webhook { Id = 3, PayloadUrl = "https://url3.com", IsActive = true, CreatedAt = DateTimeOffset.UtcNow },
+            };
+            _dbContext.Webhooks.AddRange(webhooks);
+            _dbContext.SaveChanges();
+
+            _httpMessageHandler.SetupRequest(HttpMethod.Post, webhooks[0].PayloadUrl).ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK), TimeSpan.FromMilliseconds(100));
+            _httpMessageHandler.SetupRequest(HttpMethod.Post, webhooks[1].PayloadUrl).ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK), TimeSpan.FromMilliseconds(500));
+            _httpMessageHandler.SetupRequest(HttpMethod.Post, webhooks[2].PayloadUrl).ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK), TimeSpan.FromMilliseconds(150));
+
+            // Act
+            await _controller.SendWebhooks();
+
+            // Assert
+            _httpMessageHandler.VerifyAnyRequest(Times.Exactly(3));
+            _httpMessageHandler.VerifyRequest(HttpMethod.Post, webhooks[0].PayloadUrl, async requestMessage => await MatchRequestBody(requestMessage, webhooks[0]), Times.Once());
+            _httpMessageHandler.VerifyRequest(HttpMethod.Post, webhooks[1].PayloadUrl, async requestMessage => await MatchRequestBody(requestMessage, webhooks[1]), Times.Once());
+            _httpMessageHandler.VerifyRequest(HttpMethod.Post, webhooks[2].PayloadUrl, async requestMessage => await MatchRequestBody(requestMessage, webhooks[2]), Times.Once());
         }
 
         [Fact]
@@ -103,6 +133,23 @@ namespace Webhooks.Sender.Tests.System.Controllers
                 ),
                 times
             );
+        }
+
+        private async Task<bool> MatchRequestBody(HttpRequestMessage requestMessage, Webhook webhook)
+        {
+            if (requestMessage.Content == null)
+            {
+                return false;
+            }
+
+            var json = await requestMessage.Content.ReadAsStringAsync();
+            var payload = JsonConvert.DeserializeObject<WebhookPayload>(json);
+            if (payload?.Message == null)
+            {
+                return false;
+            }
+
+            return payload.Message.Contains($"Hallo from {nameof(Webhook)} {webhook.Id.ToString(CultureInfo.InvariantCulture)}", StringComparison.InvariantCulture);
         }
     }
 }
